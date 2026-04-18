2d68d014c2b83ae554067f58f1260265bc624ff5
Matheus de Andrade Ribeiro <matheusdeandraderibeiro@gmail.com>
2026-04-18 10:58:44 -0300

    Finalização do projeto

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 import {mostraServicos} from './servicos-desktop-logica.js';
-import {mostraMenu} from './cabecalho-logica-menuLateral.js';
+//import {mostraMenu} from './cabecalho-logica-menuLateral.js';
 import {controleDestaqueSlides} from './cabecalho-logica-destaque.js';
 import {controleServicosCarrossel} from './servicos-mobile-logica.js';
 import {controleQualidadesCarrossel} from './qualidades-mobile-logica.js'
 
-mostraMenu();
+//mostraMenu();
 mostraServicos();
-controleDestaqueSlides();
+//controleDestaqueSlides();
 controleServicosCarrossel();
 controleQualidadesCarrossel();
 // window.mostraMenu = mostraMenu;
